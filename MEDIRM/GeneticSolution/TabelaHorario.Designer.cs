51d178f7adcbda120498fb55af03be652443aa75
DrG0nz0 <jefm@ua.pt>
2018-04-11 18:21:24 +0200

    format table

--- MEDIRM/GeneticSolution/TabelaHorario.Designer.cs
+++ MEDIRM/GeneticSolution/TabelaHorario.Designer.cs
@@ -29,21 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TabelaHorario));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.turnoVisualizerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.taskVisualizerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.Turno = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DiaDaSemana = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoMaquina = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Tras = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.encomendaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clienteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.artigoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TipoMaquina = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.moldeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.filmeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.papelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.encomendaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clienteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DataEntrega = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.turnoVisualizerBindingSource)).BeginInit();
@@ -57,19 +70,27 @@
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Turno,
             this.DiaDaSemana,
+            this.TipoMaquina,
             this.Column1,
             this.Tras,
-            this.encomendaDataGridViewTextBoxColumn,
-            this.clienteDataGridViewTextBoxColumn,
             this.artigoDataGridViewTextBoxColumn,
-            this.TipoMaquina,
             this.moldeDataGridViewTextBoxColumn,
             this.filmeDataGridViewTextBoxColumn,
             this.papelDataGridViewTextBoxColumn,
+            this.encomendaDataGridViewTextBoxColumn,
+            this.clienteDataGridViewTextBoxColumn,
             this.DataEntrega});
             this.dataGridView1.DataSource = this.turnoVisualizerBindingSource;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -92,6 +113,8 @@
             // Turno
             // 
             this.Turno.DataPropertyName = "Turno";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Turno.DefaultCellStyle = dataGridViewCellStyle2;
             this.Turno.HeaderText = "Turno";
             this.Turno.Name = "Turno";
             this.Turno.ReadOnly = true;
@@ -99,13 +122,26 @@
             // DiaDaSemana
             // 
             this.DiaDaSemana.DataPropertyName = "DiaDaSemana";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.DiaDaSemana.DefaultCellStyle = dataGridViewCellStyle3;
             this.DiaDaSemana.HeaderText = "DiaDaSemana";
             this.DiaDaSemana.Name = "DiaDaSemana";
             this.DiaDaSemana.ReadOnly = true;
             // 
+            // TipoMaquina
+            // 
+            this.TipoMaquina.DataPropertyName = "TipoMaquina";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.TipoMaquina.DefaultCellStyle = dataGridViewCellStyle4;
+            this.TipoMaquina.HeaderText = "TipoMaquina";
+            this.TipoMaquina.Name = "TipoMaquina";
+            this.TipoMaquina.ReadOnly = true;
+            // 
             // Column1
             // 
             this.Column1.DataPropertyName = "Frente";
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle5;
             this.Column1.HeaderText = "Frente";
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
@@ -113,41 +149,26 @@
             // Tras
             // 
             this.Tras.DataPropertyName = "Tras";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Tras.DefaultCellStyle = dataGridViewCellStyle6;
             this.Tras.HeaderText = "Tras";
             this.Tras.Name = "Tras";
             this.Tras.ReadOnly = true;
             // 
-            // encomendaDataGridViewTextBoxColumn
-            // 
-            this.encomendaDataGridViewTextBoxColumn.DataPropertyName = "Encomenda";
-            this.encomendaDataGridViewTextBoxColumn.HeaderText = "Encomenda";
-            this.encomendaDataGridViewTextBoxColumn.Name = "encomendaDataGridViewTextBoxColumn";
-            this.encomendaDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // clienteDataGridViewTextBoxColumn
-            // 
-            this.clienteDataGridViewTextBoxColumn.DataPropertyName = "Cliente";
-            this.clienteDataGridViewTextBoxColumn.HeaderText = "Cliente";
-            this.clienteDataGridViewTextBoxColumn.Name = "clienteDataGridViewTextBoxColumn";
-            this.clienteDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
             // artigoDataGridViewTextBoxColumn
             // 
             this.artigoDataGridViewTextBoxColumn.DataPropertyName = "Artigo";
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.artigoDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle7;
             this.artigoDataGridViewTextBoxColumn.HeaderText = "Artigo";
             this.artigoDataGridViewTextBoxColumn.Name = "artigoDataGridViewTextBoxColumn";
             this.artigoDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // TipoMaquina
-            // 
-            this.TipoMaquina.DataPropertyName = "TipoMaquina";
-            this.TipoMaquina.HeaderText = "TipoMaquina";
-            this.TipoMaquina.Name = "TipoMaquina";
-            this.TipoMaquina.ReadOnly = true;
-            // 
             // moldeDataGridViewTextBoxColumn
             // 
             this.moldeDataGridViewTextBoxColumn.DataPropertyName = "Molde";
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.moldeDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle8;
             this.moldeDataGridViewTextBoxColumn.HeaderText = "Molde";
             this.moldeDataGridViewTextBoxColumn.Name = "moldeDataGridViewTextBoxColumn";
             this.moldeDataGridViewTextBoxColumn.ReadOnly = true;
@@ -155,6 +176,8 @@
             // filmeDataGridViewTextBoxColumn
             // 
             this.filmeDataGridViewTextBoxColumn.DataPropertyName = "Filme";
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.filmeDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle9;
             this.filmeDataGridViewTextBoxColumn.HeaderText = "Filme";
             this.filmeDataGridViewTextBoxColumn.Name = "filmeDataGridViewTextBoxColumn";
             this.filmeDataGridViewTextBoxColumn.ReadOnly = true;
@@ -162,13 +185,35 @@
             // papelDataGridViewTextBoxColumn
             // 
             this.papelDataGridViewTextBoxColumn.DataPropertyName = "Papel";
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.papelDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle10;
             this.papelDataGridViewTextBoxColumn.HeaderText = "Papel";
             this.papelDataGridViewTextBoxColumn.Name = "papelDataGridViewTextBoxColumn";
             this.papelDataGridViewTextBoxColumn.ReadOnly = true;
             // 
+            // encomendaDataGridViewTextBoxColumn
+            // 
+            this.encomendaDataGridViewTextBoxColumn.DataPropertyName = "Encomenda";
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.encomendaDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle11;
+            this.encomendaDataGridViewTextBoxColumn.HeaderText = "Encomenda";
+            this.encomendaDataGridViewTextBoxColumn.Name = "encomendaDataGridViewTextBoxColumn";
+            this.encomendaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // clienteDataGridViewTextBoxColumn
+            // 
+            this.clienteDataGridViewTextBoxColumn.DataPropertyName = "Cliente";
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.clienteDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle12;
+            this.clienteDataGridViewTextBoxColumn.HeaderText = "Cliente";
+            this.clienteDataGridViewTextBoxColumn.Name = "clienteDataGridViewTextBoxColumn";
+            this.clienteDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
             // DataEntrega
             // 
             this.DataEntrega.DataPropertyName = "DataEntrega";
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.DataEntrega.DefaultCellStyle = dataGridViewCellStyle13;
             this.DataEntrega.HeaderText = "DataEntrega";
             this.DataEntrega.Name = "DataEntrega";
             this.DataEntrega.ReadOnly = true;
@@ -199,15 +244,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn maquinaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn Turno;
         private System.Windows.Forms.DataGridViewTextBoxColumn DiaDaSemana;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoMaquina;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Tras;
-        private System.Windows.Forms.DataGridViewTextBoxColumn encomendaDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clienteDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn artigoDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TipoMaquina;
         private System.Windows.Forms.DataGridViewTextBoxColumn moldeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn filmeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn papelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn encomendaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clienteDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn DataEntrega;
     }
 }
